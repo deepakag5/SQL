34fb3e2f92856ad47f775b3eb323d7abec5affcf
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-12 15:27:35 -0500

    added sol Q4 and Q5

--- Standford_SQL/movie_rating_db/movie_rating_query_exercises.sql
+++ Standford_SQL/movie_rating_db/movie_rating_query_exercises.sql
@@ -12,6 +12,7 @@ WHERE mID IN (SELECT mID FROM Rating WHERE stars IN (4 , 5))
 ORDER BY year;
 
 -- Q3 Find the titles of all movies that have no ratings
+
 -- query cost 2.6
 SELECT title
 FROM Movie
@@ -25,3 +26,17 @@ ON  m.mID=r.mID
 WHERE r.mID is NULL;
 
 -- second query took more time as it need to perform join
+
+--Q4 Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date.
+
+SELECT name
+FROM Reviewer
+WHERE Reviewer.rID IN (SELECT rID FROM Rating WHERE ratingDate is NULL)
+
+--Q5 Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate.
+--Also, sort the data, first by reviewername, then by movie title, and lastly by number of stars.
+
+SELECT DISTINCT name,title,stars,ratingDate
+FROM Reviewer,Movie,Rating
+WHERE Reviewer.rID=Rating.rID AND Rating.mID=Movie.mID
+ORDER BY name,title,stars
