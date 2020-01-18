ba6bc6ba8a3f17f1476f9ba9bedee224e133203c
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-17 20:43:59 -0500

    added two more sol

--- leetcode_SQL/easy_problems.sql
+++ leetcode_SQL/easy_problems.sql
@@ -1,26 +1,31 @@
 -- Find the Team Size
+
 SELECT employee_id, team_size FROM employee e
 JOIN (SELECT team_id, count(*) as team_size
       FROM employee GROUP BY team_id) as t
 ON e.team_id=t.team_id;
 
 -- Product Sales Analysis I
+
 SELECT p.product_name, s.year, s.price
 FROM Sales as s
 LEFT JOIN Product p
 ON s.product_id=p.product_id;
 
 -- Product Sales Analysis II
+
 SELECT product_id, sum(quantity) as total_quantity
 FROM Sales
 GROUP BY product_id;
 
 -- Game Play Analysis I
+
 SELECT player_id, MIN(event_date) as first_login
 FROM activity
 GROUP BY player_id
 
 -- Average Selling Price
+
 SELECT p.product_id, ROUND(SUM(units*price)/ SUM(units),2) as average_price
 FROM prices p
 JOIN unitssold u
@@ -29,6 +34,7 @@ WHERE u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
 
 -- Immediate Food Delivery I
+
 SELECT ROUND(COUNT(*)/(SELECT COUNT(*) FROM delivery)*100,2) as immediate_percentage
 FROM delivery as d
 WHERE d.order_date=d.customer_pref_delivery_date;
@@ -37,6 +43,8 @@ SELECT round(100 * SUM(order_date = customer_pref_delivery_date) / COUNT(*), 2) 
 FROM delivery;  -- sum can directly be applied to boolean in MySQL
 
 -- Reformat Department Table
+
+-- using IF
 SELECT id,
        SUM(IF(month='Jan', revenue, null)) as Jan_Revenue,
        SUM(IF(month='Feb', revenue, null)) as Feb_Revenue,
@@ -52,6 +60,8 @@ SELECT id,
        SUM(IF(month='Dec', revenue, null)) as Dec_Revenue
 FROM department
 GROUP BY id;
+
+-- using CASE WHEN
 
 SELECT id,
        SUM(CASE WHEN month='Jan' THEN revenue ELSE null END)) as Jan_Revenue,
@@ -70,17 +80,20 @@ FROM department
 GROUP BY id;
 
 -- Big Countries
+
 SELECT name, population, area
 FROM world
 WHERE area>3000000 OR population>25000000;
 
 -- Shortest Distance in a line
+
 SELECT MIN(ABS(a.x-b.x)) as shortest
 FROM point a
 JOIN point b
 ON a.x>b.x
 
 -- Article Views
+
 SELECT author_id as id
 FROM views as v
 WHERE v.author_id=v.viewer_id
@@ -88,17 +101,20 @@ GROUP BY author_id
 ORDER BY author_id;
 
 -- Swap Salary
+
 UPDATE salary SET sex = IF(sex='m','f','m')
 UPDATE salary SET sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END;
 
 
 -- Actors and Directors Who Cooperated At Least Three Times
+
 SELECT actor_id, director_id
 FROM actordirector
 GROUP BY actor_id, director_id
 HAVING COUNT(*)>=3;
 
 -- Number of Comments per Post
+
 -- using subquery
 SELECT DISTINCT s1.sub_id, (SELECT COUNT(DISTINCT sub_id) FROM submissions s2 WHERE s1.sub_id=s2.parent_id)
 FROM submissions s1
@@ -114,3 +130,24 @@ ON s1.sub_id=s2.parent_id
 WHERE s1.parent_id is null
 GROUP BY s1.sub_id
 ORDER BY s1.sub_id;
+
+-- Sales Analysis I
+
+SELECT seller_id
+FROM Sales
+GROUP BY seller_id
+HAVING SUM(price) = (SELECT SUM(price) as max_sale_price
+FROM Sales
+GROUP BY seller_id
+ORDER BY SUM(price) DESC
+LIMIT 1);
+
+-- Students and Examinations
+
+SELECT a.student_id, a.student_name, b.subject_name, COUNT(c.subject_name) as attended_exams
+FROM Students a
+JOIN Subjects b
+LEFT JOIN Examinations c
+ON a.student_id=c.student_id AND b.subject_name=c.subject_name
+GROUP BY 1,2,3
+ORDER BY 1,3;
