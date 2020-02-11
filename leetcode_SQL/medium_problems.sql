d8970949b611b32e716d8d912b44b993a2c64f08
iamDeepakA <deepakagarwal@gwu.edu>
2020-02-10 20:21:27 -0500

    added another sol for people report to given manager

--- leetcode_SQL/medium_problems.sql
+++ leetcode_SQL/medium_problems.sql
@@ -6,5 +6,33 @@ JOIN employees as e2
 ON e1.manager_id=e2.employee_id
 JOIN employees as e3
 ON e2.manager_id=e3.employee_id
-WHERE e1.employee_id!=1 AND e3.manager_id=1;
+WHERE e1.employee_id!=1 AND e3.manager_id=1;  -- e3 is manager of manager
+
+-- alternate solution
+
+SELECT employee_id
+FROM employees
+WHERE employee_id!=manager_id AND manager_id=1
+
+UNION
+
+SELECT employee_id
+FROM employees
+WHERE manager_id in (SELECT employee_id FROM employees WHERE employee_id!=manager_id AND manager_id=1)
+
+UNION
+
+SELECT employee_id
+FROM employees
+WHERE manager_id IN
+(
+SELECT employee_id
+FROM employees
+WHERE manager_id IN
+(
+SELECT employee_id
+FROM employees
+WHERE employee_id!=manager_id
+AND manager_id=1)
+)
 
