40a64d58af3ffeb31d66eb0e7ede99020c17e674
iamDeepakA <deepakagarwal@gwu.edu>
2020-02-13 20:12:55 -0500

    added last person to fit in elevator - sol 2

--- leetcode_SQL/medium_problems.sql
+++ leetcode_SQL/medium_problems.sql
@@ -133,3 +133,14 @@ ON Q.person_id=T.person_id
 WHERE T.running_weight<=1000
 ORDER BY T.running_weight DESC
 LIMIT 1
+
+-- alternate solution
+
+SELECT q1.person_name
+FROM Queue q1
+JOIN Queue q2
+ON q1.turn >= q2.turn
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1
