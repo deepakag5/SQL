617ca1b954fed76d7becae4d34acbab644f0918d
iamDeepakA <deepakagarwal@gwu.edu>
2020-02-25 06:23:39 -0500

    added monthly transactions I

--- leetcode_SQL/medium_problems.sql
+++ leetcode_SQL/medium_problems.sql
@@ -155,3 +155,15 @@ HAVING COUNT(DISTINCT c.product_key)=
 SELECT COUNT(*)
 FROM product
 )
+
+-- Monthly Transactions I
+
+select
+    left(trans_date,7) as month,
+    country,
+    count(*) as trans_count,
+    sum(case when state='approved' then 1 else 0 end) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from transactions
+group by 1,2
